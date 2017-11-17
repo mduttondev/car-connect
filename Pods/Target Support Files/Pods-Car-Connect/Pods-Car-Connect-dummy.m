f8a48f186ad76046cdba876ecdaafe68157b5262
Matthew Dutton <mduttondev@gmail.com>
2017-11-16 23:50:15 -0500

    Changing to Swift; App Version 2.0

---
+++ Pods/Target Support Files/Pods-Car-Connect/Pods-Car-Connect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Car_Connect : NSObject
+@end
+@implementation PodsDummy_Pods_Car_Connect
+@end
---
+++ Pods/Target Support Files/Pods-Car-ConnectUITests/Pods-Car-ConnectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Car_ConnectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Car_ConnectUITests
+@end
